8c2658cd1d3d7bf7d6c33de31a5ca9f91ce1e2a5
DMGDy <dylangarza1909@gmail.com>
2024-11-26 01:48:02 -0500

    [run] run script from cli

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+PROJECT="UsePackage"
+SRC="src"
+BIN="bin"
+
+if [ ! -d ${BIN} ]; then
+  echo "Creating directory ${BIN} for binary files"
+  mkdir -p ${BIN}
+fi
+
+# compile project sources
+echo "Compiling project ${PROJECT}..."
+if ! javac -d ${BIN} ${SRC}/*.java;then
+  echo "Compilation failed due to errors!"
+  exit 1
+fi
+printf "Compilation successful! now running...\n\n"
+
+# run the build in jvm
+java -cp ${BIN} ${PROJECT}
