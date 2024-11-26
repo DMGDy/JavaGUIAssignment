0d4f41d8a3546adfa0bb7fdb8484af9164369188
DMGDy <dylangarza1909@gmail.com>
2024-11-26 02:16:38 -0500

    Custom exception for weight < 0

---
+++ src/ZeroOrNegativeWeightException.java
@@ -0,0 +1,7 @@
+// Exception thrown when weight a weight is zero
+public class ZeroOrNegativeWeightException extends Exception {
+  // throw exception withe message
+  public ZeroOrNegativeWeightException() {
+    super();
+  }
+}
